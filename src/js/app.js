29738478fb80f2048484cce0a9c5199cd5bdcb66
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-03-13 14:08:38 +0200

    add header and burger

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-console.log("hi");
+const menuBtn = document.querySelector('.menu__button');
+const menuClose = document.querySelector('.menu__close');
+const menuList = document.querySelector('.menu__list');
+const menuShadow = document.querySelector('.menu--close');
+
+menuBtn.addEventListener('click', () => {
+    menuList.classList.toggle('menu__list--open');
+  menuShadow.classList.toggle('menu--open');
+  document.body.style.overflow = 'hidden';
+    console.log("open");
+});
+
+menuClose.addEventListener('click', () => {
+    menuList.classList.remove('menu__list--open');
+  menuShadow.classList.remove('menu--open');
+  document.body.style.overflow = 'auto';
+    console.log("close");
+});
+
+menuShadow.addEventListener('click', (event) => {
+    if (!menuList.contains(event.target)) {
+        menuList.classList.remove('menu__list--open');
+      menuShadow.classList.remove('menu--open');
+       document.body.style.overflow = 'auto';
+        console.log("close");
+    }
+});

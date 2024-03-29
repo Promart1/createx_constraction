0cfb5558e44c5c6d7a4eb29b741c61fb62c4968b
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-03-29 12:44:09 +0200

    add project page

--- src/js/app.js
+++ src/js/app.js
@@ -364,7 +364,30 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+////// swiper project portfolio
 
+
+var galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	 		loop: true,
+			loopedSlides: 6
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 20,
+      centeredSlides: true,
+      slidesPerView: 6,
+      touchRatio: 0.2,
+      slideToClickedSlide: true,
+			loop: true,
+      loopedSlides: 6,
+
+    });
+    galleryTop.controller.control = galleryThumbs;
+    galleryThumbs.controller.control = galleryTop;
 
 
 
